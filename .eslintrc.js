3f9e5bb8dd94c9b75426f5026a87cf363eb9740b
Tim Brust <github@timbrust.de>
2023-08-16 09:59:06 +0200

    build(deps): update dependencies, GH actions

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    root: true,
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    parserOptions: {
+      project: 'tsconfig.json'
+    }
+  };
+  
